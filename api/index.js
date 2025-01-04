4f46dc7b5088ee5a3354107beaa9c0edea9c9790
Kamran <87352040+kamran-pog@users.noreply.github.com>
2025-01-04 00:20:56 -0800

    commit

--- api/index.js
+++ api/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const Animal = require("../models/animal")
 const router = express.Router();
 
 router.get("/api", (req, res) => {
     res.status(200).json ({
         message: "Welcome to our API"
     })
+})
+
+router.get("/api/animals", async (req, res, next) => {
+    try {
+        const animals = await Animal.find()
+        res.json(animals)
+    } catch(err) {
+        next(err)
+    }
 })
 
 module.exports = router;
