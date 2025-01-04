fd2f5dec6cd87faae24e7a548c3c8bcea578ebb7
Kamran <87352040+kamran-pog@users.noreply.github.com>
2025-01-03 22:39:28 -0800

    commit

--- routers/welcome.js
+++ routers/welcome.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.json.status(200) ({
+    res.status(200).json ({
         message: "Welcome to our API"
     })
 })
